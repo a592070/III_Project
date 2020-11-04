33a8d2cb3fa338b2607e108f757f04f9f844e749
rambo0021 <rambo50713@gmail.com>
2020-11-04 11:05:38 +0800

    20201104 1105
    20201104 1105

--- src/main/java/rambo0021/controller/AccountController.java
+++ src/main/java/rambo0021/controller/AccountController.java
@@ -6,14 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import rambo0021.pojo.AccountBean;
 import rambo0021.serive.AccountService;
 
 @Controller
+@SessionAttributes (names = "userList")
 public class AccountController {
 	@Autowired@Qualifier("accountService")
 	private AccountService service;
+	@RequestMapping(path = "/accountPage")
    public String ShowAccountList(Model m) {
 	   List<AccountBean> userList = service.userList();
        m.addAttribute("userList", userList);
