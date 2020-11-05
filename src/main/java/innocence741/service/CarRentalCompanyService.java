6fc466e8d8ba4715f43ee636ac22a015ad2c0e78
innocence741 <innocence741@gmail.com>
2020-11-05 19:30:38 +0800

    交通修改

--- src/main/java/innocence741/service/CarRentalCompanyService.java
+++ src/main/java/innocence741/service/CarRentalCompanyService.java
@@ -1,5 +1,7 @@
 package innocence741.service;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +22,10 @@ public class CarRentalCompanyService {
 	
 	public boolean updateCarRentalCompanyWithoutPic(CarRentalCompany cBean) {
 		return carRentalCompanyDAO.updateCarRentalCompanyWithoutPic(cBean);
+	}
+	
+	public CarRentalCompany getCarRentalCompanyBean(BigDecimal sn_rentalcompany) {
+		return carRentalCompanyDAO.getCarRentalCompanyBean(sn_rentalcompany);
 	}
 }
 
