a3ca04d391260dfac2164b6bf6ccbc4e4f0279b9
innocence741 <innocence741@gmail.com>
2020-11-11 22:33:28 +0800

    修改OrderTable

--- src/main/java/global/pojo/OrderTable.java
+++ src/main/java/global/pojo/OrderTable.java
@@ -2,7 +2,9 @@ package global.pojo;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,8 +13,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
+import innocence741.model.T_Order_List;
 import rambo0021.pojo.AccountBean;
 
 @Entity
@@ -28,6 +33,33 @@ public class OrderTable {
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "USERNAME")
 	private AccountBean accountBean;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "order_table", cascade = CascadeType.ALL)
+//	@Transient
+	Set<T_Order_List> t_Order_Lists;
+
+	@Transient
+	T_Order_List t_Order_List; // 小訂單的Bean (Traffic)
+
+	public Set<T_Order_List> getT_Order_Lists() {
+		return t_Order_Lists;
+	}
+
+	public void setT_Order_Lists(Set<T_Order_List> t_Order_Lists) {
+		this.t_Order_Lists = t_Order_Lists;
+	}
+
+	public T_Order_List getT_Order_List() {
+		return t_Order_List;
+	}
+
+	public void setT_Order_List(T_Order_List t_Order_List) {
+		this.t_Order_List = t_Order_List;
+	}
+
+	public void addT_Order_Lists(T_Order_List tBean) {
+		this.t_Order_Lists.add(tBean);
+	}
 
 	public BigDecimal getOrder_id() {
 		return order_id;
