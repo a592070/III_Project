6964c3e72122e928ed424f6980598937beb0f80f
unknown <azaz4498@gmail.com>
2020-11-13 02:10:13 +0800

    寫IO

--- src/main/java/azaz4498/model/insertFromJson.java
+++ src/main/java/azaz4498/model/insertFromJson.java
@@ -1,21 +1,28 @@
 package azaz4498.model;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class insertFromJson {
 
 	public static void main(String[] args) throws IOException {
-		ObjectMapper objectMapper=new ObjectMapper();
-		objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
 		
-		List<Object> list= Arrays.asList(objectMapper.readValue(Paths.get("C://Users/Student/Desktop/pyWorkspace/data.json").toFile(),Object.class));
-		list.forEach(System.out::println);
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode node = mapper.readTree(new File("D:/Lab2Workspace/data.json"));
+		java.util.List<String>  titles = node.findValuesAsText("art_title");
+		java.util.List<String>  types = node.findValuesAsText("art_type");
+		for (int i = 0; i <titles.size(); i++) {
+			ArticleDAO articleDAO = new ArticleDAO();
+			
+			
+			articleDAO.newArticle(titles.get(i), typeId, content, userId);
+			System.out.println(types.get(i));
+			System.out.println(titles.get(i));
+		}
+		}
+			
+		
 	}
-
-}
