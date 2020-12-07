ef329ae556ff9d940ffd987a04a282309514cc5b
innocence741 <innocence741@gmail.com>
2020-12-08 03:07:24 +0800

    高鐵前台

---
+++ src/main/java/innocence741/controller/HighSpeedRailController.java
@@ -0,0 +1,58 @@
+package innocence741.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import innocence741.model.CarRentalCompanyVO;
+import innocence741.model.HighSpeedRail;
+import innocence741.model.HighSpeedRailDAO;
+import innocence741.service.CarRentalCompanyViewService;
+import innocence741.service.HighSpeedRailService;
+
+@Controller @Lazy
+public class HighSpeedRailController {
+	
+	@Autowired
+	HighSpeedRailService highSpeedRailService;
+	
+	
+	@RequestMapping(path = "/HsrServlet", method = RequestMethod.POST)
+	public void processAction1(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+
+        String startPoint = request.getParameter("startPoint");
+        String destination = request.getParameter("destination");
+        String departureTime = request.getParameter("departureTime");
+        
+        
+		List<HighSpeedRail> list;
+    	int price = 0;
+    	highSpeedRailService.searchHSR(startPoint, destination, departureTime);
+		list = highSpeedRailService.listHsrDO();
+		System.out.println(list.size());
+		price = highSpeedRailService.ticketPrice(startPoint, destination);
+		
+        ObjectMapper objectMapper = new ObjectMapper();
+        String ujson = objectMapper.writeValueAsString(list);
+        ujson = "[" + ujson + ",{\"price\" : " + price + "}]";
+        
+        PrintWriter out = response.getWriter();
+        out.println(ujson.toString());
+	}
+
+}
--- src/main/java/innocence741/model/HighSpeedRailDAO.java
+++ src/main/java/innocence741/model/HighSpeedRailDAO.java
@@ -11,7 +11,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Repository;
 
 
 public class HighSpeedRailDAO {
